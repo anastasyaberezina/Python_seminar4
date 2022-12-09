76463224a1415e280e8f8276e046d98f4f699bcc
Anastasiya B <gavrilovaanastasya@yandex.ru>
2022-12-09 14:02:08 +0300

    Напишите программу, которая найдёт НОК (наименьшее общее кратное) этих двух чисел

--- Seminar3.py
+++ Seminar3.py
@@ -1,12 +1,23 @@
 # Задайте два числа. Напишите программу, которая найдёт НОК (наименьшее общее кратное) этих двух чисел.
 
-a, b = 18, 48
-mult_a = [] # Раскладываем числа на простые множители
-d = 2 # Делитель
+def Mult(num):       # Раскладываем числа на простые множители
+    multipliers = []
+    d = 2            # Делитель
+    while num>1:
+        while num%d == 0:
+            multipliers.append(d)
+            num//=d
+        d+=1  
+    return multipliers
 
-while a>0:
-    while a%d == 0:
-        mult_a.append(d)
-        a//=d
-    d+=1  
-print(mult_a)      
+a, b = 18, 48
+mult_a = Mult(a)
+mult_b = Mult(b)
+
+print(mult_a)    
+print(mult_b)   
+
+res=1 # Перемножаем множители
+for i in set(mult_a).intersection(set(mult_a)):
+    res*=i
+print(res) 
